b974eb0f956b570f6cf21c3c1eacc676f309a493
David Galindo <dgalindo2310@gmail.com>
2019-03-11 15:18:03 -0700

    add model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    team = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    jerseyNumber = models.IntegerField()
+
+def __str__(self):
+    return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,31 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Player
 
 
-class Player: 
-    def __init__(self, name, age, team, position, jerseyNumber):
-        self.name = name
-        self.age = age
-        self.team = team
-        self.position = position
-        self.jerseyNumber = jerseyNumber
 
-players = [ 
-    Player('Kyrie Irving', 23, 'Boston Celtics', 'PG', 11 ),
-    Player('Russell WesBrook', 30, 'Oklahoma City Thunder', 'PG', 10 ),
-    Player('Paul George', 28,'Oklahoma City Thunder', 'SF', 13),
-    Player('James Harden', 29, 'Houston Rockets', 'SG', 13),
-    Player('Joel Embiid', 24, 'Philadelphia 76ers', 'C', 21)
-] 
 
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Hello Player</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def players_index(request):
+    players = Player.objects.all()
     return render(request, 'players/index.html', {'players': players })
+
+def players_detail(request, player_id):
+    player = Player.objects.get(id=player_id)
+    return render(request, 'players/detail.html', { 'player': player })
