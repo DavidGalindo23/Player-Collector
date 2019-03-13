8d539197b2816ef43ea0dfe324faea986948f1ad
David Galindo <dgalindo2310@gmail.com>
2019-03-13 11:11:15 -0700

    add health status

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
+
+STATUS = (
+    ('a', 'Active'),
+    ('i', 'Injured'),
+    ('r', 'Rest')
+)
+
 
 # Create your models here.
 class Player(models.Model):
@@ -14,3 +22,15 @@ class Player(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'player_id': self.id})
+
+class Healing(models.Model):
+    date = models.DateField('Status date')
+    status = models.CharField(max_length=1, choices=STATUS, default=STATUS[0][0])
+
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_status_display()} on {self.date}"
+
+    class Meta:
+        ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Player
+from .forms import HealingForm
 
 
 
+class PlayerCreate(CreateView):
+    model = Player
+    fields = '__all__'
 
+class PlayerUpdate(UpdateView):
+    model = Player
+    fields = ['age', 'team', 'jerseyNumber']
+
+class PlayerDelete(DeleteView):
+    model = Player
+    success_url = '/players/'
 
 
 # Create your views here.
@@ -20,16 +31,13 @@ def players_index(request):
 
 def players_detail(request, player_id):
     player = Player.objects.get(id=player_id)
-    return render(request, 'players/detail.html', { 'player': player })
+    healing_form = HealingForm()
+    return render(request, 'players/detail.html', { 'player': player, 'healing_form': healing_form })
 
-class PlayerCreate(CreateView):
-    model = Player
-    fields = '__all__'
-
-class PlayerUpdate(UpdateView):
-    model = Player
-    fields = ['age', 'team', 'jerseyNumber']
-
-class PlayerDelete(DeleteView):
-    model = Player
-    success_url = '/players/'
+def add_healing(request, player_id):
+    form = HealingForm(request.POST)
+    if form.is_valid: 
+        new_healing = form.save(commit=False)
+        new_healing.player_id = player_id
+        new_healing.save()
+    return redirect('detail', player_id=player_id)
